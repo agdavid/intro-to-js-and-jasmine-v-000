44909bfc1ba1fe03552b0000780263c9feb4aaeb
agdavid <antonio.g.david@gmail.com>
2016-05-19 17:03:30 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function shouting(string) {
+  var string = string.toUpperCase();
+  return string;
+}
+
+var roundDown = function(number) {
+  var floored_number = Math.floor(number);
+  return floored_number;
+};
+
+var theTruth = function() {
+  return true
+};
